c7b8a6d0fe1897de4e41d72c926595e8a6887f4b
Beatriz <beabr88@gmail.com>
2022-04-19 19:42:52 +0200

    Update card.component.ts

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit, Output, } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ContactoComponent } from '../contacto/contacto.component';
 
 
 @Component({
@@ -16,7 +18,14 @@ export class CardComponent implements OnInit {
   ngOnInit(): void {
     
   }
+  hola(){
+    console.log("hola")
+  }
 
 }
+
+
+
+
 
 
