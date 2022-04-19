6622cb1ee068de69d9c23454c4bacbec8fd06ba8
Beatriz <beabr88@gmail.com>
2022-04-19 14:05:46 +0200

    Create servicio-de-paso-de-datos.service.ts

---
+++ src/app/servicio-de-paso-de-datos.service.ts
@@ -0,0 +1,17 @@
+
+
+/*ESTE SERVCIO LO USARE DE PUENTE PARA PASAR LOS DATOS DEL FORMULARIO A LA CARD*/
+
+
+import { EventEmitter, Injectable, Output } from '@angular/core';//importo el decorador Output y eventEmitter
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicioDePasoDeDatosService {
+
+
+
+  constructor() { }
+}
